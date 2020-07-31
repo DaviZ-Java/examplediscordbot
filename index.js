f5952be68510ba6b0fa9815e408bb95210e629b1
DaviZ-Java <52458432+DaviZ-Java@users.noreply.github.com>
2020-07-31 12:12:59 -0300

    Update index.js

--- index.js
+++ index.js
@@ -91,7 +91,7 @@ bot.on('message', message=>{
 
 bot.on('guildMemberAdd', member => {
     // Send the message to a designated channel on a server:
-    const channel = member.guild.channels.cache.find(ch => ch.name === 'member-log');
+    const channel = member.guild.channels.cache.find(ch => ch.name === '𝙜𝙚𝙣𝙚𝙧𝙖𝙡');
     // Do nothing if the channel wasn't found on this server
     if (!channel) return;
     // Send the message, mentioning the member
