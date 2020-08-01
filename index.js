ed2e81894b8112b9cc2e541275720d4513199454
DaviZ-Java <52458432+DaviZ-Java@users.noreply.github.com>
2020-08-01 15:12:56 -0300

    hey

--- index.js
+++ index.js
@@ -109,4 +109,20 @@ bot.on('guildMemberAdd', member => {
     channel.send(`welcome sexy i am the server hoe god, ${member}`);
   });  
 //end of welcome to new members
+//join vc
+bot.on('message', async message => {
+    // Voice only works in guilds, if the message does not come from a guild,
+    // we ignore it
+    if (!message.guild) return;
+  
+    if (message.content === '$join') {
+      // Only try to join the sender's voice channel if they are in one themselves
+      if (message.member.voice.channel) {
+        const connection = await message.member.voice.channel.join();
+      } else {
+        message.reply('join a vc, retard <3');
+      }
+    }
+  });
+//end of join vc
 bot.login(process.env.token);   
