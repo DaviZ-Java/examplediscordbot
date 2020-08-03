0a38141eef0a99080dbd216b1a4731de5b41426a
DaviZ-Java <52458432+DaviZ-Java@users.noreply.github.com>
2020-08-03 10:53:02 -0300

    Update index.js

--- index.js
+++ index.js
@@ -102,37 +102,4 @@ bot.on('message', message=>{
     }
 })
 //end of reply to certain words
-//join vc
-bot.on('message', async message => {
-    // Voice only works in guilds, if the message does not come from a guild,
-    // we ignore it
-    if (!message.guild) return;
-  
-    if (message.content === '/join') {
-      // Only try to join the sender's voice channel if they are in one themselves
-      if (message.member.voice.channel) {
-        const connection = await message.member.voice.channel.join();
-      } else {
-        message.reply('You need to join a voice channel first!');
-      }
-    }
-  });
-//end of join vc
-//bot streaming
-bot.on('ready', () => {
-    bot.user.setStatus('available')
-    bot.user.setPresence({
-        game: {
-            name: 'with depression',
-            type: "STREAMING",
-            url: "pornhub.com"
-        }
-    });
-});
-
-bot.user.setActivity("with depression", {
-    type: "STREAMING",
-    url: "pornhub.com"
-  });
-//bot streaming
 bot.login(process.env.token);   
